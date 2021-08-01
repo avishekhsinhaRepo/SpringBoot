bd4c2fa4e95b5e1c510988978e98df7f8f47c8af
Avishekh Sinha <avishekh_sinha@optum.com>
2021-08-01 21:53:06 +0530

    Caching and Swagger

---
+++ productrestapi/src/main/java/com/udemy/spring/product/config/ProductCacheConfig.java
@@ -0,0 +1,14 @@
+package com.udemy.spring.product.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.hazelcast.config.Config;
+
+@Configuration
+public class ProductCacheConfig {
+	
+	/*
+	 * public Config cacheConfig() { return new Config().
+	 * setInstanceName("hazle-instance").addMapConfig(mapConfig) }
+	 */
+}
